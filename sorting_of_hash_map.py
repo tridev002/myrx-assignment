6f37fb052b7c6e47bc4776d0511fb09e6ecf189f
tridev002 <hanumatridev999@gmail.com>
2025-02-15 12:24:24 +0530

    document uploaded

--- sorting_of_hash_map.py
+++
@@ -1,7 +0,0 @@
-def sort_dict_by_value(input_dict):
-    return dict(sorted(input_dict.items(), key=lambda item: item[1]))
-
-
-my_map= {101: "John Doe", 102: "Jane Smith", 103: "Peter Johnson"}
-sorted_map = sort_dict_by_value(my_map)
-print(sorted_map)
